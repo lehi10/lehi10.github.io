1a3ef5c154e9dd555423b3064ad5174da43b6110
LehiQr <lehi_10@hotmail.com>
2025-02-11 13:24:57 -0500

    feat: porfolio theme applied

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), react()],
   site: 'https://lehi10.github.io/',
   output: 'static',
   base: '/',
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
